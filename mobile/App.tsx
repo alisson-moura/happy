d6ac8da80558f04165af5dd4d28d6e0a75598e45
alisson-moura <alisson.mo.moura@outlook.com.br>
2020-10-15 17:35:10 -0300

    Criado tela de orfanatos e configuração das rotas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,17 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
+
+import Routes from './src/routes';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded] = useFonts({ Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold });
+  if (!fontsLoaded) {
+    return null;
+  }
+  return <Routes />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
